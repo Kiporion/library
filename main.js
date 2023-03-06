364a1007c375392be6eade9662f15d6def731c2e
Kiporion <meho.mustafic@outlook.com>
2023-03-06 12:10:31 +0100

    refactored to use Class instead of Constructor Function

--- main.js
+++ main.js
@@ -2,11 +2,12 @@
 
 let myLibrary = [];
 // Main Object
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
+class Book {
+  constructor (title, author, pages, read){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;}
 }
 
 Book.prototype.toggleRead = function () {
